cdbb9430accb67f19bd545051561f6614e2e87ef
BishalBhujel <bishalbhujel112@gmail.com>
2020-10-02 23:06:25 +0545

    starting

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Nav } from './Navabr.elements';
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                test
+            </Nav>   
+        </>
+    )
+}
+
+export default Navbar;
